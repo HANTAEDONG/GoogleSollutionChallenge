4acf98c3b7046d39fac8f3f30bffb5baf0c60688
BooHyeongJin <154498718+BooHyeongJin@users.noreply.github.com>
2024-01-17 15:58:43 +0900

    Create register_frame.js

---
+++ front/register(react+css)/register_frame.js
@@ -0,0 +1,112 @@
+import "./Frame.css";
+
+const Frame = () => {
+  return (
+    <div className="div">
+      <div className="div1">
+        <img
+          className="icround-arrow-back-ios-icon"
+          alt=""
+          src="/icroundarrowbackios.svg"
+        />
+        <div className="div2">
+          <img
+            className="icround-arrow-back-ios-icon"
+            alt=""
+            src="/icroundarrowbackios.svg"
+          />
+          <img className="icon" alt="" src="/.svg" />
+          <div className="div3">
+            <b className="text">사진첨부하기</b>
+          </div>
+        </div>
+        <div className="div4">
+          <img
+            className="icround-arrow-back-ios-icon"
+            alt=""
+            src="/icroundarrowbackios.svg"
+          />
+          <div className="div5">
+            <b className="text">카테고리설정</b>
+          </div>
+          <div className="div6">
+            <div className="wrapper">
+              <div className="div7">제목을 입력해주세요</div>
+            </div>
+          </div>
+          <div className="div8">
+            <b className="text">제목 및 카테고리</b>
+          </div>
+        </div>
+        <div className="div9">
+          <img
+            className="icround-arrow-back-ios-icon"
+            alt=""
+            src="/icroundarrowbackios.svg"
+          />
+          <div className="div10">
+            <div className="wrapper">
+              <div className="div7">가격을 입력해주세요</div>
+            </div>
+          </div>
+          <div className="div12">
+            <b className="text">가격</b>
+          </div>
+        </div>
+        <div className="div13">
+          <img
+            className="icround-arrow-back-ios-icon"
+            alt=""
+            src="/icroundarrowbackios.svg"
+          />
+          <div className="div14">
+            <div className="frame">
+              <div className="div7">위치 설정하기</div>
+            </div>
+            <img
+              className="icround-arrow-back-ios-icon5"
+              alt=""
+              src="/icroundarrowbackios.svg"
+            />
+          </div>
+          <div className="div16">
+            <b className="text">거래희망지역</b>
+          </div>
+        </div>
+        <div className="div17">
+          <img
+            className="icround-arrow-back-ios-icon"
+            alt=""
+            src="/icroundarrowbackios.svg"
+          />
+          <div className="div18">
+            <div className="wrapper">
+              <div className="div7">상품 설명을 입력해주세요</div>
+            </div>
+          </div>
+          <div className="div20">
+            <b className="text">상품 설명</b>
+          </div>
+        </div>
+        <div className="parent">
+          <div className="div21">
+            <div className="div22" />
+          </div>
+          <div className="div23">
+            <b className="text6">등록하기</b>
+          </div>
+        </div>
+        <div className="group">
+          <div className="div21">
+            <div className="div22" />
+          </div>
+          <div className="div23">
+            <b className="text6">취소하기</b>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Frame;
