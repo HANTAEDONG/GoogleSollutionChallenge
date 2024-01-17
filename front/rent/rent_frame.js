6db43e22baa04673e23f59056c583516af2238d2
BooHyeongJin <154498718+BooHyeongJin@users.noreply.github.com>
2024-01-17 16:18:29 +0900

    Create rent_frame.js

---
+++ front/rent/rent_frame.js
@@ -0,0 +1,274 @@
+import styled from "styled-components";
+
+const Text1 = styled.b`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  top: 0%;
+  left: 0%;
+  line-height: 22px;
+  display: inline-block;
+`;
+const Div = styled.div`
+  position: absolute;
+  top: 11px;
+  left: calc(50% - 337.5px);
+  background-color: var(--color-gainsboro-100);
+  width: 675px;
+  height: 310px;
+`;
+const Div1 = styled.div`
+  position: absolute;
+  top: 583px;
+  left: 0px;
+  background-color: var(--color-white);
+  border: 1px solid var(--color-darkgray);
+  box-sizing: border-box;
+  width: 715px;
+  height: 372px;
+  overflow: hidden;
+`;
+const Icon = styled.img`
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 715px;
+  height: 439px;
+  overflow: hidden;
+`;
+const Icon1 = styled.img`
+  position: absolute;
+  top: calc(50% - 25px);
+  left: 20px;
+  width: 50px;
+  height: 50px;
+`;
+const Text2 = styled.b`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  top: 0%;
+  left: 0%;
+  display: flex;
+  align-items: center;
+`;
+const Div2 = styled.div`
+  position: absolute;
+  top: calc(50% - 14px);
+  left: 80px;
+  background-color: var(--color-gainsboro-100);
+  width: 206px;
+  height: 28px;
+`;
+const Div3 = styled.div`
+  position: absolute;
+  height: calc(100% - 4px);
+  width: calc(100% - 4px);
+  top: 2px;
+  right: 2px;
+  bottom: 2px;
+  left: 2px;
+  background-color: var(--color-gray);
+  overflow: hidden;
+`;
+const Div4 = styled.div`
+  position: absolute;
+  top: calc(50% - 20px);
+  left: 600px;
+  background-color: var(--color-gainsboro-100);
+  width: 95px;
+  height: 40px;
+  overflow: hidden;
+`;
+const Div5 = styled.div`
+  position: absolute;
+  top: calc(50% - 38.5px);
+  left: calc(50% - 357.5px);
+  border: 1px solid var(--color-darkgray);
+  box-sizing: border-box;
+  width: 715px;
+  height: 64px;
+  overflow: hidden;
+  font-size: 20px;
+`;
+const Div6 = styled.div`
+  position: absolute;
+  top: 9px;
+  left: calc(50% - 337.5px);
+  background-color: var(--color-gainsboro-100);
+  width: 410px;
+  height: 40px;
+  font-size: 25px;
+`;
+const Div7 = styled.div`
+  position: absolute;
+  top: 54px;
+  left: calc(50% - 337.5px);
+  background-color: var(--color-gainsboro-100);
+  width: 70px;
+  height: 18px;
+`;
+const Div8 = styled.div`
+  position: absolute;
+  top: 54px;
+  left: calc(50% - 249.5px);
+  background-color: var(--color-gainsboro-100);
+  width: 70px;
+  height: 18px;
+`;
+const Div9 = styled.div`
+  position: absolute;
+  top: 54px;
+  left: calc(50% - 161.5px);
+  background-color: var(--color-gainsboro-100);
+  width: 70px;
+  height: 18px;
+`;
+const Div10 = styled.div`
+  position: absolute;
+  top: 54px;
+  left: calc(50% + 171.5px);
+  background-color: var(--color-gainsboro-100);
+  width: 50px;
+  height: 18px;
+  text-align: right;
+`;
+const Div11 = styled.div`
+  position: absolute;
+  top: 54px;
+  left: calc(50% + 229.5px);
+  background-color: var(--color-gainsboro-100);
+  width: 50px;
+  height: 18px;
+  text-align: right;
+`;
+const Div12 = styled.div`
+  position: absolute;
+  top: 54px;
+  left: calc(50% + 287.5px);
+  background-color: var(--color-gainsboro-100);
+  width: 50px;
+  height: 18px;
+  text-align: right;
+`;
+const IcroundArrowBackIosIcon = styled.img`
+  position: absolute;
+  top: 54px;
+  left: 90px;
+  width: 18px;
+  height: 18px;
+  overflow: hidden;
+  object-fit: contain;
+`;
+const Div13 = styled.div`
+  position: absolute;
+  top: 503px;
+  left: 0px;
+  background-color: var(--color-white);
+  border: 1px solid var(--color-darkgray);
+  box-sizing: border-box;
+  width: 715px;
+  height: 80px;
+  overflow: hidden;
+`;
+const IcroundArrowBackIosIcon1 = styled.img`
+  position: absolute;
+  top: 555px;
+  left: 178px;
+  width: 18px;
+  height: 18px;
+  overflow: hidden;
+  object-fit: contain;
+`;
+const Div14 = styled.div`
+  position: absolute;
+  height: calc(100% - 69px);
+  top: 71px;
+  bottom: -2px;
+  left: calc(50% - 387px);
+  background-color: var(--color-white);
+  border: 1px solid var(--color-darkgray);
+  box-sizing: border-box;
+  width: 715px;
+  overflow: hidden;
+`;
+const DivRoot = styled.div`
+  position: relative;
+  background-color: var(--color-white);
+  width: 100%;
+  height: 1024px;
+  overflow-y: auto;
+  text-align: left;
+  font-size: var(--font-size-mini);
+  color: var(--color-darkgray);
+  font-family: var(--font-roboto);
+`;
+
+const Frame = () => {
+  return (
+    <DivRoot>
+      <Div14>
+        <Div1>
+          <Div>
+            <Text1>
+              밤새 기온이 크게 떨어지면서 오늘 아침은 전국적으로 강추위가 기승을
+              부렸습니다.강원도 철원군의 아침 최저 기온은 영하 18.4도로 전국
+              최저를 기록했습니다.경기 파주 영하 12.9도, 서울도 영하 6.9도로
+              어제보다 5에서 최대 10도가량 기온이 낮았습니다.찬 바람이 더해져
+              전북 무주군 산간의 체감 온도는 영하 23.8도, 서울에서도 영하
+              11.8도까지 곤두박질쳤습니다.포천과 파주 등 경기 5개 시군과 평창,
+              철원 등 강원 8개 시군, 강원 산간으로는 한파 주의보가 내려져
+              있습니다.낮에는 기온이 영상으로 올라서겠지만, 내일 아침에 다시
+              서울 영하 7도, 대관령 영하 13도까지 떨어져 예년 이맘때 수준의
+              추위가 나타나겠습니다.바깥 활동을 할 때는 모자와 장갑 등
+              방한용품을 챙겨 보온에 신경을 써야 합니다.또 최근 내린 눈과 비가
+              얼어붙어 곳곳에 빙판길과 도로 살얼음이 예상돼 미끄럼 사고에도
+              주의가 필요합니다.이번 추위는 내일 아침까지 이어진 뒤 서서히
+              풀리겠습니다.중북부 지방에 내려진 한파 특보도 내일 오전 중에는
+              모두 해제될 전망입니다.주 중반 이후엔 영동을 중심으로 눈비가, 남부
+              지방으로는 겨울비가 내릴 전망입니다.지금까지 보도국에서
+              전해드렸습니다.
+            </Text1>
+          </Div>
+        </Div1>
+        <Icon alt="" src="/-.svg" />
+        <Div5>
+          <Icon1 alt="" src="/.svg" />
+          <Div2>
+            <Text2>ID</Text2>
+          </Div2>
+          <Div4>
+            <Div3 />
+          </Div4>
+        </Div5>
+        <Div13>
+          <Div6>
+            <Text2>이러이러한 물건을 빌려드립니다</Text2>
+          </Div6>
+          <Div7>
+            <Text2>카테고리1</Text2>
+          </Div7>
+          <Div8>
+            <Text2>카테고리2</Text2>
+          </Div8>
+          <Div9>
+            <Text2>카테고리3</Text2>
+          </Div9>
+          <Div10>
+            <Text2>어디시</Text2>
+          </Div10>
+          <Div11>
+            <Text2>어디구</Text2>
+          </Div11>
+          <Div12>
+            <Text2>어디동</Text2>
+          </Div12>
+          <IcroundArrowBackIosIcon alt="" src="/icroundarrowbackios.svg" />
+        </Div13>
+        <IcroundArrowBackIosIcon1 alt="" src="/icroundarrowbackios.svg" />
+      </Div14>
+    </DivRoot>
+  );
+};
+
+export default Frame;
