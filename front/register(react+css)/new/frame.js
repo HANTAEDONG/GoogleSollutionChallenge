a26b251d6290a6b3b8afff9a6b7a5b02d991a4b4
BooHyeongJin <154498718+BooHyeongJin@users.noreply.github.com>
2024-01-21 17:16:36 +0900

    Create frame.js

---
+++ front/register(react+css)/new/frame.js
@@ -0,0 +1,417 @@
+import styled from "styled-components";
+
+const IcroundArrowBackIosIcon = styled.img`
+  position: absolute;
+  top: 555px;
+  left: 178px;
+  width: 18px;
+  height: 18px;
+  overflow: hidden;
+  object-fit: contain;
+`;
+const Icon = styled.img`
+  position: absolute;
+  top: calc(50% - 29px);
+  left: 14px;
+  width: 100px;
+  height: 100px;
+  overflow: hidden;
+`;
+const Text = styled.b`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  top: 0%;
+  left: 0%;
+  display: flex;
+  align-items: center;
+`;
+const Div = styled.div`
+  position: absolute;
+  top: 10px;
+  left: 14px;
+  background-color: var(--color-gainsboro);
+  width: 116px;
+  height: 34px;
+`;
+const Div1 = styled.div`
+  position: absolute;
+  height: calc(100% - 754px);
+  top: 0px;
+  bottom: 754px;
+  left: calc(50% - 358px);
+  background-color: var(--color-white);
+  border: 1px solid var(--color-darkgray);
+  box-sizing: border-box;
+  width: 716px;
+  overflow: hidden;
+`;
+const Div2 = styled.div`
+  position: absolute;
+  top: calc(50% - 9px);
+  left: 577px;
+  background-color: var(--color-gainsboro);
+  width: 112px;
+  height: 47px;
+`;
+const Div3 = styled.div`
+  flex: 1;
+  position: relative;
+  line-height: 20px;
+  opacity: 0.2;
+`;
+const Wrapper = styled.div`
+  flex: 1;
+  height: 24px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+`;
+const Div4 = styled.div`
+  position: absolute;
+  top: 46px;
+  left: calc(50% - 344px);
+  border-radius: var(--br-5xs);
+  background-color: var(--color-whitesmoke-100);
+  border: 1px solid var(--color-whitesmoke-200);
+  box-sizing: border-box;
+  width: 538px;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: flex-start;
+  padding: var(--padding-xs) var(--padding-base);
+  font-size: var(--font-size-lg);
+  color: var(--color-gray-100);
+  font-family: var(--font-arial);
+`;
+const Div5 = styled.div`
+  position: absolute;
+  top: 5px;
+  left: 14px;
+  background-color: var(--color-gainsboro);
+  width: 145px;
+  height: 34px;
+`;
+const Div6 = styled.div`
+  position: absolute;
+  height: calc(100% - 838px);
+  top: 194px;
+  bottom: 644px;
+  left: calc(50% - 358px);
+  background-color: var(--color-white);
+  border: 1px solid var(--color-darkgray);
+  box-sizing: border-box;
+  width: 716px;
+  overflow: hidden;
+`;
+const Div7 = styled.div`
+  position: absolute;
+  top: 52px;
+  left: calc(50% - 344px);
+  border-radius: var(--br-5xs);
+  background-color: var(--color-whitesmoke-100);
+  border: 1px solid var(--color-whitesmoke-200);
+  box-sizing: border-box;
+  width: 538px;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: flex-start;
+  padding: var(--padding-xs) var(--padding-base);
+`;
+const Div8 = styled.div`
+  position: absolute;
+  top: 10px;
+  left: 14px;
+  background-color: var(--color-gainsboro);
+  width: 56px;
+  height: 34px;
+  font-size: var(--font-size-xl);
+  color: var(--color-darkgray);
+  font-family: var(--font-roboto);
+`;
+const Div9 = styled.div`
+  position: absolute;
+  height: calc(100% - 838px);
+  top: 304px;
+  bottom: 534px;
+  left: calc(50% - 358px);
+  background-color: var(--color-white);
+  border: 1px solid var(--color-darkgray);
+  box-sizing: border-box;
+  width: 716px;
+  overflow: hidden;
+  font-size: var(--font-size-lg);
+  color: var(--color-gray-100);
+  font-family: var(--font-arial);
+`;
+const Div10 = styled.div`
+  position: absolute;
+  top: 50px;
+  left: calc(50% - 344px);
+  border-radius: var(--br-5xs);
+  background-color: var(--color-whitesmoke-100);
+  border: 1px solid var(--color-whitesmoke-200);
+  box-sizing: border-box;
+  width: 678px;
+  height: 246px;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: flex-start;
+  padding: var(--padding-xs) var(--padding-base);
+`;
+const Div11 = styled.div`
+  position: absolute;
+  top: 10px;
+  left: 14px;
+  background-color: var(--color-gainsboro);
+  width: 86px;
+  height: 34px;
+  font-size: var(--font-size-xl);
+  color: var(--color-darkgray);
+  font-family: var(--font-roboto);
+`;
+const Div12 = styled.div`
+  position: absolute;
+  height: calc(100% - 633px);
+  top: 414px;
+  bottom: 219px;
+  left: calc(50% - 358px);
+  background-color: var(--color-white);
+  border: 1px solid var(--color-darkgray);
+  box-sizing: border-box;
+  width: 716px;
+  overflow: hidden;
+  font-size: var(--font-size-lg);
+  color: var(--color-gray-100);
+  font-family: var(--font-arial);
+`;
+const FrameDiv = styled.div`
+  width: 312px;
+  height: 24px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+`;
+const IcroundArrowBackIosIcon1 = styled.img`
+  width: 35px;
+  position: relative;
+  height: 35px;
+  overflow: hidden;
+  flex-shrink: 0;
+  object-fit: contain;
+`;
+const Div13 = styled.div`
+  position: absolute;
+  top: 52px;
+  left: calc(50% - 344px);
+  border-radius: var(--br-5xs);
+  background-color: var(--color-whitesmoke-100);
+  border: 1px solid var(--color-whitesmoke-200);
+  box-sizing: border-box;
+  width: 377px;
+  height: 48px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  padding: var(--padding-xs) 4px;
+`;
+const Div14 = styled.div`
+  position: absolute;
+  top: 10px;
+  left: 14px;
+  background-color: var(--color-gainsboro);
+  width: 117px;
+  height: 34px;
+  font-size: var(--font-size-xl);
+  color: var(--color-darkgray);
+  font-family: var(--font-roboto);
+`;
+const Div15 = styled.div`
+  position: absolute;
+  height: calc(100% - 838px);
+  top: 729px;
+  bottom: 109px;
+  left: calc(50% - 358px);
+  background-color: var(--color-white);
+  border: 1px solid var(--color-darkgray);
+  box-sizing: border-box;
+  width: 716px;
+  overflow: hidden;
+  font-size: var(--font-size-lg);
+  color: var(--color-gray-100);
+  font-family: var(--font-arial);
+`;
+const Div16 = styled.div`
+  position: absolute;
+  height: calc(100% - 4px);
+  width: calc(100% - 4px);
+  top: 2px;
+  right: 2px;
+  bottom: 2px;
+  left: 2px;
+  background-color: var(--color-gray-200);
+  overflow: hidden;
+`;
+const Div17 = styled.div`
+  width: 156px;
+  position: relative;
+  background-color: var(--color-gainsboro);
+  height: 56px;
+  overflow: hidden;
+  flex-shrink: 0;
+  z-index: 0;
+`;
+const Text1 = styled.b`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  top: 0%;
+  left: 0%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const Div18 = styled.div`
+  width: 148px;
+  position: absolute;
+  margin: 0 !important;
+  top: 4px;
+  left: 4px;
+  background-color: var(--color-gainsboro);
+  height: 48px;
+  z-index: 1;
+`;
+const Parent = styled.div`
+  position: absolute;
+  top: 868px;
+  left: 140px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  gap: var(--gap-3xs);
+  text-align: center;
+`;
+const Group = styled.div`
+  position: absolute;
+  top: 868px;
+  left: 420px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  gap: var(--gap-3xs);
+  text-align: center;
+`;
+const Div19 = styled.div`
+  position: absolute;
+  height: calc(100% - 76px);
+  top: 76px;
+  bottom: 0px;
+  left: calc(50% - 365px);
+  background-color: var(--color-white);
+  border: 1px solid var(--color-darkgray);
+  box-sizing: border-box;
+  width: 716px;
+  overflow-y: auto;
+`;
+const DivRoot = styled.div`
+  width: 100%;
+  position: relative;
+  background-color: var(--color-white);
+  height: 1024px;
+  overflow: hidden;
+  text-align: left;
+  font-size: var(--font-size-xl);
+  color: var(--color-darkgray);
+  font-family: var(--font-roboto);
+`;
+
+const Frame = () => {
+  return (
+    <DivRoot>
+      <Div19>
+        <IcroundArrowBackIosIcon alt="" src="/icroundarrowbackios.svg" />
+        <Div1>
+          <IcroundArrowBackIosIcon alt="" src="/icroundarrowbackios.svg" />
+          <Icon alt="" src="/.svg" />
+          <Div>
+            <Text>사진첨부하기</Text>
+          </Div>
+        </Div1>
+        <Div6>
+          <IcroundArrowBackIosIcon alt="" src="/icroundarrowbackios.svg" />
+          <Div2>
+            <Text>카테고리설정</Text>
+          </Div2>
+          <Div4>
+            <Wrapper>
+              <Div3>제목을 입력해주세요</Div3>
+            </Wrapper>
+          </Div4>
+          <Div5>
+            <Text>제목 및 카테고리</Text>
+          </Div5>
+        </Div6>
+        <Div9>
+          <IcroundArrowBackIosIcon alt="" src="/icroundarrowbackios.svg" />
+          <Div7>
+            <Wrapper>
+              <Div3>가격을 입력해주세요</Div3>
+            </Wrapper>
+          </Div7>
+          <Div8>
+            <Text>가격</Text>
+          </Div8>
+        </Div9>
+        <Div12>
+          <IcroundArrowBackIosIcon alt="" src="/icroundarrowbackios.svg" />
+          <Div10>
+            <Wrapper>
+              <Div3>상품 설명을 입력해주세요</Div3>
+            </Wrapper>
+          </Div10>
+          <Div11>
+            <Text>상품 설명</Text>
+          </Div11>
+        </Div12>
+        <Div15>
+          <IcroundArrowBackIosIcon alt="" src="/icroundarrowbackios.svg" />
+          <Div13>
+            <FrameDiv>
+              <Div3>위치 설정하기</Div3>
+            </FrameDiv>
+            <IcroundArrowBackIosIcon1 alt="" src="/icroundarrowbackios.svg" />
+          </Div13>
+          <Div14>
+            <Text>거래희망지역</Text>
+          </Div14>
+        </Div15>
+        <Parent>
+          <Div17>
+            <Div16 />
+          </Div17>
+          <Div18>
+            <Text1>등록하기</Text1>
+          </Div18>
+        </Parent>
+        <Group>
+          <Div17>
+            <Div16 />
+          </Div17>
+          <Div18>
+            <Text1>취소하기</Text1>
+          </Div18>
+        </Group>
+      </Div19>
+    </DivRoot>
+  );
+};
+
+export default Frame;
